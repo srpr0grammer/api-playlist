07209df5849a692fcc44a51d509ee06aa8a8b7a6
alan <alnjsantos@hotmail.com>
2023-10-02 22:03:04 +0100

    initial commit

---
+++ api-playlist/src/main/java/com/apiplaylist/ApiPlaylistApplication.java
@@ -0,0 +1,13 @@
+package com.apiplaylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPlaylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPlaylistApplication.class, args);
+	}
+
+}
